071fdf2d45ec2ee2845934ebb41b97ca2c6925d7
Mati Czarnecki <maticzacza@gmail.com>
2022-06-19 20:33:23 +0200

    initial commit

---
+++ InitialScripts/app.js
@@ -0,0 +1,14 @@
+const shoppingCart = [
+  {
+    product: "jenga",
+    price: 19.99,
+    quantity: 1,
+  },
+  {
+    product: "Klocki",
+    price: 9.99,
+    quantity: 9,
+  },
+];
+
+console.log(shoppingCart[1]["price"]);
