85777d89da459f536364c2344c98617e1eb68d9e
Mati Czarnecki <maticzacza@gmail.com>
2022-06-27 22:45:50 +0200

    onclick property

--- 03 DOMEvents/app.js
+++ 03 DOMEvents/app.js
@@ -1,1 +1,18 @@
+const btn = document.querySelector("#v2");
+console.dir(btn);
 
+btn.onclick = () => {
+  console.log("you clicked me!!");
+  console.log("i hope it worked!!");
+};
+
+function scream() {
+  console.log("AAAAAAAAAA");
+  console.log("stop touching me!!");
+}
+
+btn.onmouseenter = scream;
+
+h1 = document.querySelector("h1").onclick = () => {
+  alert("you clicked me hm");
+};
