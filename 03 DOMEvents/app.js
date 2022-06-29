d61c167bd988fa9cecb498c63509642020727adf
Mati Czarnecki <maticzacza@gmail.com>
2022-06-29 12:58:59 +0200

    addEventListener()

--- 03 DOMEvents/app.js
+++ 03 DOMEvents/app.js
@@ -1,3 +1,5 @@
+// onclick property
+
 const btn = document.querySelector("#v2");
 console.dir(btn);
 
@@ -16,3 +18,25 @@ btn.onmouseenter = scream;
 h1 = document.querySelector("h1").onclick = () => {
   alert("you clicked me hm");
 };
+
+// addEventListener
+const btn3 = document.querySelector("#v3");
+btn3.addEventListener("mouseup", () => {
+  alert("Button 3 clicked!");
+});
+
+function twist() {
+  console.log("TWIST!");
+}
+
+function shout() {
+  console.log("SHOUT!");
+}
+
+const tasButton = document.querySelector("#tas");
+tasButton.onclick = twist;
+tasButton.onclick = shout;
+
+// in addEventListener we can run more than one function on one propety
+tasButton.addEventListener("click", twist, { once: true });
+tasButton.addEventListener("click", shout);
