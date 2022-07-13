1286c22e5cb3b66b809fcd2759bee9832672359d
Mati Czarnecki <maticzacza@gmail.com>
2022-07-13 11:37:31 +0200

    promises intro) q q!

--- 05 Async JavaScript/app.js
+++ 05 Async JavaScript/app.js
@@ -9,6 +9,40 @@ const fakeRequestCallback = (url, success, failure) => {
     }
   }, delay);
 };
+// Callback hell
+// fakeRequestCallback(
+//   "books.com/page1",
+//   function (response) {
+//     console.log("it worked!");
+//     console.log(response);
+//     fakeRequestCallback(
+//       "books.com/page2",
+//       function (response) {
+//         console.log("it worked again!");
+//         console.log(response);
+//         fakeRequestCallback(
+//           "books.com/page3",
+//           function (response) {
+//             console.log("it worked again (3 request)");
+//             console.log(response);
+//           },
+//           function (err) {
+//             console.log("error (3 request)");
+//             console.log(err);
+//           }
+//         );
+//       },
+//       function (err) {
+//         console.log("error (2nd request)!");
+//       }
+//     );
+//   },
+//   function (response) {
+//     console.log("error!");
+//     console.log(response);
+//   }
+// );
+
 // THE PROMISE VERSION
 const fakeRequestPromise = (url) => {
   return new Promise((resolve, reject) => {
@@ -23,35 +57,26 @@ const fakeRequestPromise = (url) => {
   });
 };
 
-fakeRequestCallback(
-  "books.com/page1",
-  function (response) {
+fakeRequestPromise("yelp.com/api/coffee/page1")
+  .then(() => {
+    console.log("PROMISE RESOLVED!");
     console.log("it worked!");
-    console.log(response);
-    fakeRequestCallback(
-      "books.com/page2",
-      function (response) {
-        console.log("it worked again!");
-        console.log(response);
-        fakeRequestCallback(
-          "books.com/page3",
-          function (response) {
-            console.log("it worked again (3 request)");
-            console.log(response);
-          },
-          function (err) {
-            console.log("error (3 request)");
-            console.log(err);
-          }
-        );
-      },
-      function (err) {
-        console.log("error (2nd request)!");
-      }
-    );
-  },
-  function (response) {
-    console.log("error!");
-    console.log(response);
-  }
-);
+    fakeRequestPromise("yelp.com/api/coffee/page2")
+      .then(() => {
+        console.log("it worked! (2nd)");
+        fakeRequestPromise("yelp.com/api/coffee/page3")
+          .then(() => {
+            console.log("it worked! (3nd)");
+          })
+          .catch(() => {
+            console.log("oh no, error! (3nd)");
+          });
+      })
+      .catch(() => {
+        console.log("oh no, error! (2nd)");
+      });
+  })
+  .catch(() => {
+    console.log("PROMISE REJECTED!");
+    console.log("oh no, error!");
+  });
