e8678d53c347090a20be7cc87263f0d6f8257698
Mati Czarnecki <maticzacza@gmail.com>
2022-07-13 13:29:09 +0200

    await Keyword

---
+++ 05 Async JavaScript/awaitKeyword.js
@@ -0,0 +1,48 @@
+const delayedColorChange = (color, delay) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      document.body.style.backgroundColor = color;
+      resolve();
+    }, delay);
+  });
+};
+
+async function rainbow() {
+  await delayedColorChange("red", 1000);
+  await delayedColorChange("orange", 1000);
+  await delayedColorChange("yellow", 1000);
+  await delayedColorChange("green", 1000);
+  await delayedColorChange("blue", 1000);
+  await delayedColorChange("indigo", 1000);
+  await delayedColorChange("violet", 1000);
+  return "all done!";
+}
+
+rainbow().then(() => {
+  console.log("end of rainbow");
+});
+
+async function printRainbow() {
+  await rainbow();
+  console.log("end of rainbow");
+}
+
+// fakeRequest example
+const fakeRequest = (url) => {
+  return new Promise((resolve, reject) => {
+    const delay = Math.floor(Math.random() * 4500) + 500;
+    setTimeout(() => {
+      if (delay > 4000) {
+        reject("Connection Timeout :(");
+      } else {
+        resolve(`Here is your fake data from ${url}`);
+      }
+    }, delay);
+  });
+};
+
+// awaiting for request and storing it in variable
+async function makeTwoRequests() {
+  let data1 = await fakeRequest("/page1");
+  console.log(data1);
+}
