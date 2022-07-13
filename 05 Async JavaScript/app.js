d379e3f6e41efafea9b74a193b23b3127cdfe197
Mati Czarnecki <maticzacza@gmail.com>
2022-07-13 11:21:35 +0200

    callback hell - without promises

--- 05 Async JavaScript/app.js
+++ 05 Async JavaScript/app.js
@@ -1,22 +1,57 @@
+// THE CALLBACK VERSION
 const fakeRequestCallback = (url, success, failure) => {
   const delay = Math.floor(Math.random() * 4500) + 500;
   setTimeout(() => {
     if (delay > 4000) {
-      failure("Connection Timeout");
+      failure("Connection Timeout :(");
     } else {
       success(`Here is your fake data from ${url}`);
     }
   }, delay);
 };
+// THE PROMISE VERSION
+const fakeRequestPromise = (url) => {
+  return new Promise((resolve, reject) => {
+    const delay = Math.floor(Math.random() * 4500) + 500;
+    setTimeout(() => {
+      if (delay > 4000) {
+        reject("Connection Timeout :(");
+      } else {
+        resolve(`Here is your fake data from ${url}`);
+      }
+    }, delay);
+  });
+};
 
 fakeRequestCallback(
-  "books.com",
+  "books.com/page1",
   function (response) {
-    console.log("it worked");
+    console.log("it worked!");
     console.log(response);
+    fakeRequestCallback(
+      "books.com/page2",
+      function (response) {
+        console.log("it worked again!");
+        console.log(response);
+        fakeRequestCallback(
+          "books.com/page3",
+          function (response) {
+            console.log("it worked again (3 request)");
+            console.log(response);
+          },
+          function (err) {
+            console.log("error (3 request)");
+            console.log(err);
+          }
+        );
+      },
+      function (err) {
+        console.log("error (2nd request)!");
+      }
+    );
   },
   function (response) {
-    console.log("error! ");
+    console.log("error!");
     console.log(response);
   }
 );
