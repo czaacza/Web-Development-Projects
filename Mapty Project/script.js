a2b959879bf5e28729bf93176de7c8d3ca61a3a2
Mati Czarnecki <maticzacza@gmail.com>
2022-07-15 14:10:42 +0200

    mapty project design changed, refactorization into classes, default date

--- Mapty Project/script.js
+++ Mapty Project/script.js
@@ -5,47 +5,142 @@ const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 
 
 const form = document.querySelector('.form');
 const containerWorkouts = document.querySelector('.workouts');
-const inputType = document.querySelector('.form__input--type');
-const inputDistance = document.querySelector('.form__input--distance');
-const inputDuration = document.querySelector('.form__input--duration');
-const inputCadence = document.querySelector('.form__input--cadence');
-const inputElevation = document.querySelector('.form__input--elevation');
+const inputName = document.querySelector('.form__input--name');
+const inputDifficulty = document.querySelector('.form__input--difficulty');
+const inputSize = document.querySelector('.form__input--size');
+const inputTerrain = document.querySelector('.form__input--terrain');
+const inputDate = document.querySelector('.form__input--date');
 
-if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(
-    function (position) {
-      const { latitude } = position.coords;
-      const { longitude } = position.coords;
-      const coords = [latitude, longitude];
-      console.log(`https://www.google.pl/maps/@${latitude},${longitude},13z`);
+class Geopoint {
+  id = (Date.now() + '').slice(-10);
 
-      var map = L.map('map').setView(coords, 13);
+  constructor(coords, name, difficulty, size, terrain, date) {
+    this.coords = coords;
+    this.name = name;
+    this.difficulty = difficulty;
+    this.size = size;
+    this.terrain = terrain;
+    this.date = date;
+  }
+}
 
-      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution:
-          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-      }).addTo(map);
+////////////////////////////
+// APPLICATION ARCHITECTURE
+////////////////////////////
+class App {
+  #map;
+  #newMarker;
 
-      map.on('click', function (mapEvent) {
-        console.log(mapEvent);
-        const { lat, lng } = mapEvent.latlng;
-        L.marker([lat, lng])
-          .addTo(map)
-          .bindPopup(
-            L.popup({
-              maxWidth: 300,
-              minWidth: 100,
-              autoClose: false,
-              closeOnClick: false,
-              className: 'easy-popup',
-            })
-          )
-          .setPopupContent('Geocache point')
-          .openPopup();
-      });
-    },
-    function () {
-      alert('Could not get your position');
+  constructor() {
+    this._getPosition();
+
+    form.addEventListener('submit', this._newGeopoint.bind(this));
+  }
+
+  _getPosition() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        this._loadMap.bind(this),
+        function () {
+          alert('Could not get your position');
+        }
+      );
     }
-  );
+  }
+
+  _loadMap(position) {
+    const { latitude } = position.coords;
+    const { longitude } = position.coords;
+    const coords = [latitude, longitude];
+
+    this.#map = L.map('map').setView(coords, 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(this.#map);
+
+    // Handling clicks on #map
+    this.#map.on('click', mapEvent => {
+      this._showForm();
+      clearInputs();
+      if (this.#newMarker) {
+        this.#newMarker.remove();
+      }
+
+      const { lat, lng } = mapEvent.latlng;
+
+      this.#newMarker = L.marker([lat, lng]).addTo(this.#map);
+    });
+  }
+
+  _showForm() {
+    form.classList.remove('hidden');
+    inputName.focus();
+  }
+
+  _newGeopoint(e) {
+    e.preventDefault();
+
+    let popupContent = 'Geocache point';
+    if (inputName.value.length > 0) {
+      popupContent = inputName.value;
+    }
+    this.#newMarker
+      .bindPopup(
+        L.popup({
+          maxWidth: 300,
+          minWidth: 100,
+          autoClose: false,
+          closeOnClick: false,
+          className: addPopupClassName(inputDifficulty.value),
+        })
+      )
+      .setPopupContent(popupContent)
+      .openPopup();
+
+    console.log(inputDate.value);
+    this.#newMarker = undefined;
+    form.classList.add('hidden');
+  }
+}
+
+const app = new App();
+// help functions
+function addPopupClassName(difficulty) {
+  let popupClassName;
+  if (difficulty <= 2) {
+    popupClassName = 'easy-popup';
+  } else if (difficulty == 3) {
+    popupClassName = 'medium-popup';
+  } else if (difficulty <= 5) {
+    popupClassName = 'hard-popup';
+  }
+  return popupClassName;
+}
+
+function clearInputs() {
+  inputName.value = '';
+  inputDifficulty.value = '';
+  inputSize.value = '';
+  inputTerrain.value = '';
+  inputDate.value = getTodayDate();
+}
+
+function getTodayDate() {
+  var today = new Date();
+  var dd = today.getDate();
+  var mm = today.getMonth() + 1; //January is 0!
+  var yyyy = today.getFullYear();
+
+  if (dd < 10) {
+    dd = '0' + dd;
+  }
+
+  if (mm < 10) {
+    mm = '0' + mm;
+  }
+
+  today = yyyy + '-' + mm + '-' + dd;
+  return today;
 }
