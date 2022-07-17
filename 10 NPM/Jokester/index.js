e65e9e517bc74b1ffd322ffb0af0ace0cee53a07
Mati Czarnecki <maticzacza@gmail.com>
2022-07-17 13:17:10 +0200

    npm ended

--- 10 NPM/Jokester/index.js
+++ 10 NPM/Jokester/index.js
@@ -1,6 +1,8 @@
 const jokes = require("give-me-a-joke");
 const colors = require("colors");
+const cowsay = require("cowsay");
 console.dir(jokes);
+console.dir(cowsay);
 jokes.getRandomDadJoke(function (joke) {
   console.log(joke.rainbow);
 });
---
+++ 10 NPM/LanguageDetector/index.js
@@ -0,0 +1,6 @@
+const franc = require("franc");
+// const langs = require("langs");
+
+// const sentence = argv[2] || "Hello World";
+// const languageCode = franc(sentence);
+// console.log(languageCode);
