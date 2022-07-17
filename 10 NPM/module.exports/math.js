d8f2420753e75f7e6f608818001a6d816838776d
Mati Czarnecki <maticzacza@gmail.com>
2022-07-17 12:11:28 +0200

    first brush with node ended, nmp started

---
+++ 10 NPM/module.exports/math.js
@@ -0,0 +1,8 @@
+exports.add = (x, y) => x + y;
+exports.PI = 3.14159;
+exports.square = (x) => x * x;
+
+//
+// module.exports.add = add;
+// module.exports.PI = PI;
+// module.exports.square = square;
