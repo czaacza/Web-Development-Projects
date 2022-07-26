9670906a1f8c9c2457233c0ec0afb8e353858585
Mati Czarnecki <maticzacza@gmail.com>
2022-07-26 12:28:57 +0200

    parsing data to templates

--- WebDeveloperBootcamp/12 Dynamic HTML - Templating/index.js
+++ WebDeveloperBootcamp/12 Dynamic HTML - Templating/index.js
@@ -10,6 +10,11 @@ app.get("/", (req, res) => {
   res.render("home.ejs");
 });
 
+app.get("/random", (req, res) => {
+  const num = Math.floor(Math.random() * 10) + 1;
+  res.render("random", { num });
+});
+
 app.listen(3000, () => {
   console.log("LISTENING ON PORT 3000");
 });
