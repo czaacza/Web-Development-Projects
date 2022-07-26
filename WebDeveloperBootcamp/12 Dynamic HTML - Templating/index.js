6e840e47d9c11f57a620a5d8cd5f9648982a79ea
Mati Czarnecki <maticzacza@gmail.com>
2022-07-26 14:33:08 +0200

    more complex subreddit template demo

--- WebDeveloperBootcamp/12 Dynamic HTML - Templating/index.js
+++ WebDeveloperBootcamp/12 Dynamic HTML - Templating/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const redditData = require("./data.json");
 
 app.set("view engine", "ejs");
 
@@ -19,7 +20,12 @@ app.get("/cats", (req, res) => {
 // PARSING DATA TO TEMPLATES
 app.get("/r/:subreddit", (req, res) => {
   const { subreddit } = req.params;
-  res.render("subreddit", { subreddit });
+  const data = redditData[subreddit];
+  if (data) {
+    res.render("subreddit", { ...data });
+  } else {
+    res.render("notfound", { subreddit });
+  }
 });
 
 // IF STATEMENTS, PARSING DATA
